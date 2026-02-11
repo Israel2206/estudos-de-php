0a05ef8a5c60fe72dc6d620a081eb5397f8be0a3
Breno Israel <brenoisrael931@gmail.com>
2026-02-11 13:36:24 -0300

    concatenação

---
+++ fundamentos-basicos/04-concatenacao.php
@@ -0,0 +1,7 @@
+<?php
+$meuNome = 'Breno Israel';
+echo 'Meu nome é $meuNome<br>'; //Aspas simples são no literal
+echo 'Meu nome é '.$meuNome.' e minha idade é 23'; //Contatenação
+echo"<hr>";
+echo "Meu nome é $meuNome";
+?>
