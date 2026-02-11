439b5c64a565f5b258d3b487a4208896fd1ffb5e
Breno Israel <brenoisrael931@gmail.com>
2026-02-11 14:31:25 -0300

    laços condicionais

---
+++ fundamentos-basicos/06-condicionais.php
@@ -0,0 +1,13 @@
+<?php
+//condicionais
+$numero = 22;
+
+if ($numero == 10) {
+    echo "O número é 10";
+} elseif ($numero %2 == 0){
+    echo "O número é par";
+} else {
+    echo "O número é impar";
+}
+
+?>
---
+++ fundamentos-basicos/07-switch.php
@@ -0,0 +1,13 @@
+<?php
+//switch
+$cor = "verde";
+
+switch ($cor) {
+    case 'verde':
+        echo"Essa cor é maza";
+        break;
+    default:
+        echo"Essa cor n é maza";
+        break;
+}
+?>
